ccaf2651accaf7109e82a3a601079cc7597f3ec0
ahmad <ahmad@fedora-t440.local>
2019-10-22 00:11:57 +0400

    created LUG db model

--- lugs/models.py
+++ lugs/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Lug(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    website = models.URLField()
+    # logo = models.ImageField()
+    # todo make it a list
+    country = models.CharField(max_length=100)
+    province = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    # location on mape
+    # occurrence
+    contact_person = models.CharField(max_length=200)
+    # todo: make a list
+    contact_info = models.TextField()
+    donate_link = models.URLField()
+    added_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_added = models.DateTimeField(default=timezone.now)
