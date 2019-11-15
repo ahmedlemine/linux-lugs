078a213ddeaa17c85e3b4a5d8bd2c83e847ff8d2
Ahmed Lemine <aljakany@gmail.com>
2019-11-15 23:10:32 +0400

    switched profile image to use ImgKit for processing

--- users/models.py
+++ users/models.py
@@ -2,12 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 from lugs.models import Lug
 from PIL import Image
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
 from cities_light.models import City
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     real_name = models.CharField(max_length=100, null=True, blank=True, default="")
-    image = models.ImageField(default='default.jpeg', upload_to='profile_pics')
+    # image = models.ImageField(default='default.jpeg', upload_to='profile_pics')
+    image = ProcessedImageField(verbose_name='Profile Photo', default='default.jpeg', upload_to='profile_pics', blank=True,
+                                           processors=[ResizeToFill(128, 128)],
+                                           format='JPEG',
+                                           options={'quality': 60})
     city = models.ForeignKey(City, on_delete=models.CASCADE, null=True, blank=True)
     lugs = models.ManyToManyField(Lug)
     twitter = models.URLField(default="", null=True, blank=True)
@@ -18,13 +24,13 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
     
-    #fix for profile not created issue
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
+    # #fix for profile not created issue
+    # def save(self, *args, **kwargs):
+    #     super().save(*args, **kwargs)
 
-        img = Image.open(self.image.path)
+    #     img = Image.open(self.image.path)
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+    #     if img.height > 300 or img.width > 300:
+    #         output_size = (300, 300)
+    #         img.thumbnail(output_size)
+    #         img.save(self.image.path)
